cb802e5b9cfabaf68ec695d3cfa568e09e718a27
HHKimryanov19 <63718770+HHKimryanov19@users.noreply.github.com>
2026-04-09 15:15:34 +0300

    Add initial HTML structure

---
+++ todo-app/src/App.tsx
@@ -0,0 +1,46 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <section>
+      <select>
+        <option value="option1">Option 1</option>
+        <option value="option2">Option 2</option>
+        <option value="option3">Option 3</option>
+      </select>
+      <select>
+        <option value="option1">Option 1</option>
+        <option value="option2">Option 2</option>
+        <option value="option3">Option 3</option>
+      </select>
+      <div>
+        <p>Pending</p>
+        <ul>
+          <li>Task 1</li>
+          <li>Task 2</li>
+          <li>Task 3</li> 
+        </ul>
+      </div>
+    </section>
+    <section>
+      <select>
+        <option value="option1">Option 1</option>
+        <option value="option2">Option 2</option>
+        <option value="option3">Option 3</option>
+      </select>
+      <div>
+        <p>Pending</p>
+        <ul>
+          <li>Task 1</li>
+          <li>Task 2</li>
+          <li>Task 3</li>
+        </ul>
+      </div>
+    </section>
+    </>
+  )
+}
+
+
+export default App
